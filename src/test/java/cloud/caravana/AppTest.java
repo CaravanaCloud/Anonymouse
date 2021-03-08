4f5fe9c1818c8a0bb3e09a1770c0eea096dba0cc
Julio Faerman <jfaerman@gmail.com>
2021-03-08 22:47:27 +0100

    hello

---
+++ src/test/java/cloud/caravana/AppTest.java
@@ -0,0 +1,12 @@
+package cloud.caravana;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
